c98f993d84628afd2fe22a7677091df02de2af64
Greg Beecroft <Greg.Beecroft@rightscale.com>
2013-04-26 18:22:18 -0600

    Initial test

--- cookbooks/escapetest/metadata.rb
+++ cookbooks/escapetest/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures escapetest'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+recipe "escapetest::default", "Testing escape chars"
