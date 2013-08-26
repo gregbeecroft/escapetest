9a538fde58b0584cd7fa73e8a01c36a1b2b521be
Greg Beecroft <Greg.Beecroft@rightscale.com>
2013-08-26 16:19:16 -0600

    Forgot metadata, testing hello world again

--- cookbooks/escapetest/metadata.rb
+++ cookbooks/escapetest/metadata.rb
@@ -7,3 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 recipe "escapetest::default", "Testing escape chars"
+recipe "escapetest::hello_world", "Testing Hello World"
